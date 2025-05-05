c721d6625c2a92c74ec6909efccfe8d64d923c17
Nakibul Hassan <nakibhasan2711@gmail.com>
2025-05-05 20:29:09 +0600

    - added proper folder structuring

--- app/src/main/java/com/nakibul/android/noteappclean/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/nakibul/android/noteappclean/feature_note/presentation/MainActivity.kt
@@ -1,16 +1,9 @@
-package com.nakibul.android.noteappclean
+package com.nakibul.android.noteappclean.feature_note.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.nakibul.android.noteappclean.ui.theme.NoteAppCLEANTheme
 
 class MainActivity : ComponentActivity() {
